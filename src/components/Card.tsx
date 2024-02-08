1db9581ccc33959db0466bfa79efb20f3c848fa5
Diego Nunes <diegonunesabc@gmail.com>
2024-02-08 17:53:54 -0300

    More Changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,16 +19,16 @@ const Card = ({ Course, img, Color, Name, Brand }: CardProps) => {
                        <img src={pinImage} />
                     </div>
                     <div className="text-block-2">
-                        <div className="course-wrapper">{Course}</div>
+                        <div className="course-wrapper">{Course}tranquily Trails</div>
                     </div>
                 </div>
                 <img src={imageLogo} loading="lazy" alt="${disc.Disc}" className="image" />
                 <div className="w-layout-grid grid grid-disc">
                     <div className="course-list">
                         <ul>
-                            <li><i className='bx bx-palette' /><span>{Color}</span></li>
-                            <li><i className='bx bxs-user-detail' /><span>{Name}</span></li>
-                            <li><i className='bx bx-purchase-tag' /><span>{Brand}</span></li>                                      
+                            <li><i className='bx bx-palette' /><span>{Color}Green Raptor</span></li>
+                            <li><i className='bx bxs-user-detail' /><span>{Name}C. Deck</span></li>
+                            <li><i className='bx bx-purchase-tag' /><span>{Brand}MVP</span></li>                                      
                         </ul>
                         <button className="btn-2">Claim Disc</button>
                     </div>                             
