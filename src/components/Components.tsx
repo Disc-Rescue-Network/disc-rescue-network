b6de18070375da3b98dbcfa5913861b06a3a609c
Chris Deck <deckchris95@gmail.com>
2024-02-08 10:52:31 -0500

    initial start

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -3,6 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/globals.css";
 
 //This is where we will load a single page of all the different components used in the app
-export default function Home() {
-  return <p>This is the Home page</p>;
+export default function Components() {
+  return <p>This is the components page</p>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import LoadingScreen from "./components/LoadingScreen";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/globals.css";
 
