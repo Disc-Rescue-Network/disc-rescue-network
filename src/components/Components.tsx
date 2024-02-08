f6a5fc7769b0700c1efc52f2e04c47cf40d47d4b
Diego Nunes <diegonunesabc@gmail.com>
2024-02-08 18:23:00 -0300

    any changes

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/globals.css";
 import Footer from "./Footer";
-import Card from "./Card";
 
 //This is where we will load a single page of all the different components used in the app
 export default function Components() {
@@ -10,7 +9,6 @@ export default function Components() {
     <>
       <p>This is the components page</p>
       <Footer needCutOut={true} />
-      <Card Course={""} Color={""} Name={""} Brand={""} img={""} />
     </>
   );
 }
