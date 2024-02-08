9d030c99c43b8dcff638f596c7e863937a6ec85b
Chris Deck <deckchris95@gmail.com>
2024-02-08 17:01:41 -0500

    button is done

--- src/views/Components.tsx
+++ src/views/Components.tsx
@@ -7,17 +7,26 @@ import Card from "../components/Card";
 //This is where we will load a single page of all the different components used in the app
 export default function Components() {
   const showToast = () => {
-    alert("This is a toast");
+    alert("Button clicked");
   };
 
   return (
-    <>
+    <div className="container">
       <p>This is the components page</p>
-      <Card Course={""} Color={""} Name={""} Brand={""} img={""} />
-      <Card Course={""} Color={""} Name={""} Brand={""} img={""} />
-      <Button text={"This is a red button"} onClick={showToast} red={true} />
-      <Button text={"This is a blue button"} onClick={showToast} red={false} />
+      <div className="card-container-row">
+        <Card Course={""} Color={""} Name={""} Brand={""} img={""} />
+        <Card Course={""} Color={""} Name={""} Brand={""} img={""} />
+      </div>
+
+      <div className="btn-container">
+        <Button text={"This is a red button"} onClick={showToast} red={true} />
+        <Button
+          text={"This is a blue button"}
+          onClick={showToast}
+          red={false}
+        />
+      </div>
       <Footer needCutOut={true} />
-    </>
+    </div>
   );
 }
