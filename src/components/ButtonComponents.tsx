2ffabb80dcc1b46deaf171b1664d9df3f8a5d0cc
Diego Nunes <diegonunesabc@gmail.com>
2024-02-12 14:58:18 -0300

    Button-components and headerComponents.css

---
+++ src/components/ButtonComponents.tsx
@@ -0,0 +1,36 @@
+import "../styles/buttonComponents.css"
+import Button from "./Button"
+import imgFind from "../assets/search.png"
+import imgSearch from "../assets/courses.png"
+
+
+const ButtonComponents = () => {
+    const showToast = () => {
+        alert("Button clicked");
+      };
+
+    return (
+         <div className="btn-container-components min-height-35">
+            <div className="box">
+             <Button
+                text={"FIND MY DISC"} 
+                onClick={showToast} 
+                red={true} 
+                className="button-home"
+            />
+            <div className="btn-description">GO THROUGH THE <span className="rescue-flow" onClick={showToast}>RESCUE FLOW</span> TO FIND YOUR DISC</div>
+            </div> 
+            <div className="box">
+             <Button 
+                text={"SEARCH COURSES"}
+                onClick={showToast}
+                red={false} 
+                className="button-home"
+            />
+            <div className="btn-description">SEARCH BY COURSE FOR TURNED IN DISCS</div>
+            </div>
+         </div>
+    )
+}
+
+export default ButtonComponents
