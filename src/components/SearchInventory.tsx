76f3c10fd219452849800d0d97290e120551ae91
Chris Deck <deckchris95@gmail.com>
2024-02-14 11:41:21 -0500

    refactoring inventory component to accept params

--- src/components/SearchInventory.tsx
+++ src/components/SearchInventory.tsx
@@ -1,25 +1,28 @@
-import "../styles/searchInventory.css"
-import LogoSearch from "../assets/icon_logo_transparent_fullsize.png"
+import "../styles/searchInventory.css";
+import LogoSearch from "../assets/icon_logo_transparent_fullsize.png";
 import SubHeaderSearch from "./SubHeaderSearch";
 
-
 const SearchAll = () => {
-    const handleLogoClick = () => {
-        window.location.reload();
-      };
-    return (
-        <>
-        <div  className="logo-search">
-            <img 
-                className="icon-logo-search" 
-                src={LogoSearch} 
-                alt="Logo Search All" 
-                onClick={handleLogoClick}
-            />
-        </div>
-        <SubHeaderSearch CourseName={"@ Tranquility Trails"} />
-        </>
-    )
-}
+  const handleLogoClick = () => {
+    window.location.reload();
+  };
+  return (
+    <>
+      <div className="logo-search">
+        <img
+          className="icon-logo-search"
+          src={LogoSearch}
+          alt="Logo Search All"
+          onClick={handleLogoClick}
+        />
+      </div>
+      <SubHeaderSearch
+        CourseName={"Tranquility Trails"}
+        baseText="All Lost"
+        lightText="Discs"
+      />
+    </>
+  );
+};
 
-export default SearchAll
+export default SearchAll;
