15753b0db77d011115fc9fbb26aeee220e1fa937
Chris Deck <deckchris95@gmail.com>
2024-04-08 14:34:34 -0400

    fixing rescue flow

--- src/views/RescueFlow.tsx
+++ src/views/RescueFlow.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import BetaBanner from "../components/BetaBanner";
 import RescueFlowStep1 from "../components/RescueFlowStep1";
+import RescueFlowStep2 from "../components/RescueFlowStep2";
+import RescueFlowStep3 from "../components/RescueFlowStep3";
+import RescueFlowStep4 from "../components/RescueFlowStep4";
+import RescueFlowStep5 from "../components/RescueFlowStep5";
 
 export default function RescueFlow() {
+  const [step, setStep] = useState(1);
+
   return (
     <div className="container-rescue">
       <BetaBanner Course={"This is a Beta version of the DRN Platform."} />
-      <RescueFlowStep1 />
+      {step === 1 && <RescueFlowStep1 step={step} setStep={setStep} />}
+      {step === 2 && <RescueFlowStep2 />}
+      {step === 3 && <RescueFlowStep3 />}
+      {step === 4 && <RescueFlowStep4 />}
+      {step === 5 && <RescueFlowStep5 />}
     </div>
   );
 }
