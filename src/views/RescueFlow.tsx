c188c2a8df9bc5659143e427c9ae8e336119205d
Diego Nunes <diegonunesabc@gmail.com>
2024-04-08 18:26:07 -0300

    Arrow and Wizardbox in the rescueFlow

--- src/views/RescueFlow.tsx
+++ src/views/RescueFlow.tsx
@@ -1,22 +1,39 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import BetaBanner from "../components/BetaBanner";
 import RescueFlowStep1 from "../components/RescueFlowStep1";
 import RescueFlowStep2 from "../components/RescueFlowStep2";
 import RescueFlowStep3 from "../components/RescueFlowStep3";
 import RescueFlowStep4 from "../components/RescueFlowStep4";
 import RescueFlowStep5 from "../components/RescueFlowStep5";
+import Arrow from "../assets/arrow-down.png";
 
 export default function RescueFlow() {
   const [step, setStep] = useState(1);
 
+  const handleBack = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
   return (
     <div className="container-rescue">
+      <i className="arrow-left-icon" onClick={handleBack}> 
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </i>
       <BetaBanner Course={"This is a Beta version of the DRN Platform."} />
       {step === 1 && <RescueFlowStep1 step={step} setStep={setStep} />}
       {step === 2 && <RescueFlowStep2 />}
       {step === 3 && <RescueFlowStep3 />}
       {step === 4 && <RescueFlowStep4 />}
       {step === 5 && <RescueFlowStep5 />}
+      <div className="wizardbox">
+        <img src={Arrow} alt="arrow"/>
+        <p>if you don’t want to use the wizard you can always just 
+          enter the information manually on our search page</p>
+      </div>
     </div>
   );
 }
