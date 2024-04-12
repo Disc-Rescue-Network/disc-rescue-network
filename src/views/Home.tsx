4061be5ddd1a8a1502ed25728dcaf41860a498ba
Trey Coon <coontrey@gmail.com>
2024-04-12 10:29:14 -0400

    finished getting inventory

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,109 +7,25 @@ import Discs from "../components/Discs";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
 import { Disc, DiscStateString } from "../App";
+import axios from "axios";
 
-// const arrayOfDiscsHome = [
-//   {
-//     course: "Tranquility Trails",
-//     color: "Green",
-//     name: "D. Bryant",
-//     discAndBrand: "Discraft Buzz",
-//   },
-//   {
-//     course: "Stafford Woods",
-//     color: "Blue",
-//     name: "A. Nichols",
-//     discAndBrand: "MVP Volt",
-//   },
-//   {
-//     course: "Tranquility Trails",
-//     color: "Yellow",
-//     name: "C. Deck",
-//     discAndBrand: "Axiom Crave",
-//   },
-//   {
-//     course: "Doc Cramer",
-//     color: "Red",
-//     name: "J. Doe",
-//     discAndBrand: "Innova Roc3",
-//   },
-//   {
-//     course: "Tranquility Trails",
-//     color: "Yellow",
-//     name: "C. Deck",
-//     discAndBrand: "Axiom Crave",
-//   },
-//   {
-//     course: "Doc Cramer",
-//     color: "Red",
-//     name: "J. Doe",
-//     discAndBrand: "Innova Roc3",
-//   },
-//   {
-//     course: "Tranquility Trails",
-//     color: "Yellow",
-//     name: "C. Deck",
-//     discAndBrand: "Axiom Crave",
-//   },
-// ];
 
-//This is the actual home page of the app
+
 
 function Home() {
   const [allDiscs, setAllDiscs] = useState<Disc[]>([]);
-  const tempDiscs = [
-    {
-      id: 7,
-      course: "DeLaveaga",
-      name: "R. Cornelius",
-      disc: "Buzzz",
-      phoneNumber: "9991234567",
-      bin: "1",
-      dateFound: "2023-01-03",
-      dateTexted: "",
-      dateClaimed: "2023-10-03",
-      status: DiscStateString.Unclaimed,
-      comments: "Light blue big z",
-      color: "",
-      claimBy: "2024-06-03",
-      brand: "Innova",
-    },
-    {
-      id: 8,
-      course: "Girdwood Forest Fair Park",
-      name: "B. Lin",
-      disc: "Meta essence",
-      phoneNumber: "9991234567",
-      bin: "1",
-      dateFound: "2023-01-03",
-      dateTexted: "2023-10-07",
-      dateClaimed: "",
-      status: DiscStateString.Unclaimed,
-      comments: "",
-      color: "Blue",
-      claimBy: "2024-06-04",
-      brand: "Innova",
-    },
-    {
-      id: 9,
-      course: "Tranquility Trails",
-      name: "Archie B.",
-      disc: "Explorer",
-      phoneNumber: "9991234567",
-      bin: "1",
-      dateFound: "2023-01-03",
-      dateTexted: "2023-10-07",
-      dateClaimed: "",
-      status: DiscStateString.New,
-      comments: "",
-      color: "Yellow",
-      claimBy: "2024-06-04",
-      brand: "Birdie",
-    },
-  ];
 
   useEffect(() => {
-    setAllDiscs(tempDiscs);
+    const fetchDiscs = async () => {
+      try {
+        const response = await axios.get('https://api.discrescuenetwork.com/inventory');
+        setAllDiscs(response.data); // Assuming the API response directly contains the array of discs
+      } catch (error) {
+        console.error('Failed to fetch discs:', error);
+      }
+    };
+
+    fetchDiscs();
   }, []);
 
   return (
