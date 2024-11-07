d147b43548218d128f010f0b590f8e5f0f2bbcba
Chris Deck <deckchris95@gmail.com>
2024-11-07 15:59:43 -0500

    prod URL

--- src/App.tsx
+++ src/App.tsx
@@ -96,8 +96,8 @@ export enum DiscStateString {
   Surrendered = "SURRENDERED",
 }
 
-//export const API_BASE_URL = "https://apis.discrescuenetwork.com"; //production URL
-export const API_BASE_URL = "http://localhost:8080"; // local testing
+export const API_BASE_URL = "https://apis.discrescuenetwork.com"; //production URL
+//export const API_BASE_URL = "http://localhost:8080"; // local testing
 
 function App() {
   const { inventory, fetchInventory } = useInventory();
