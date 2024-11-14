a7992e7c4cb74c6f8aa903e313ae2822db3e8600
Chris Deck <deckchris95@gmail.com>
2024-11-14 13:08:47 -0500

    TOF update

--- src/components/TermsOfFlow.tsx
+++ src/components/TermsOfFlow.tsx
@@ -23,29 +23,35 @@ const TermsOfFlow: React.FC<TermsOfFlowProps> = (props: TermsOfFlowProps) => {
         </span>
         <h2 className="header-surrender-disc">Terms of Service (TOS)</h2>
         <p className="content-report-popup">
-          <ol>
-            <li>
+          <ol
+            style={{
+              textAlign: "left",
+              marginLeft: "10px",
+              paddingLeft: "5px",
+            }}
+          >
+            <li style={{ marginBottom: "10px" }}>
               <strong>Ownership Verification:</strong> By claiming a disc, you
               attest that you are the rightful owner. Claiming a disc that does
               not belong to you is strictly prohibited and constitutes theft.
             </li>
-            <li>
+            <li style={{ marginBottom: "10px" }}>
               <strong>Waiver of Responsibility:</strong> Disc Rescue Network
               does not assume responsibility for verifying ownership beyond
               information provided by the claimant. Users are responsible for
               ensuring that claims are accurate and made in good faith.
             </li>
-            <li>
+            <li style={{ marginBottom: "10px" }}>
               <strong>Liability for Wrongful Claims:</strong> Any individual
               claiming a disc they do not own may be held accountable by the
               disc golf course or rightful owner. Wrongful claims may result in
               prosecution, as this action is treated as theft.
             </li>
-            <li>
+            <li style={{ marginBottom: "10px" }}>
               <strong>Course Enforcement:</strong> Disc golf courses reserve the
               right to prosecute individuals who make wrongful claims on discs.
             </li>
-            <li>
+            <li style={{ marginBottom: "10px" }}>
               <strong>Acknowledgment of Terms:</strong> By using the Disc Rescue
               Network service, you agree to these terms and acknowledge the
               consequences of falsely claiming a disc.
