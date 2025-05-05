56856ddb858f7765b4404c90d112a5fad7d940fc
Chris Deck <deckchris95@gmail.com>
2025-05-05 17:10:01 -0400

    UI updates to beta banner. can be dismissed. new text.

--- src/components/BetaBanner.tsx
+++ src/components/BetaBanner.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
 type BetaBanner = {
   Text: string;
   LinkText: String;
@@ -6,6 +10,19 @@ type BetaBanner = {
 
 export default function BetaBanner(props: BetaBanner) {
   const { Text, LinkText, SecondText } = props;
+  const [isVisible, setIsVisible] = useState(true);
+
+  const hideBanner = () => {
+    setIsVisible(false);
+  };
+
+  // We don't persist this state to localStorage
+  // It will reset when the page refreshes
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div className="beta-banner">
       <div className="content-beta-banner">
@@ -19,6 +36,13 @@ export default function BetaBanner(props: BetaBanner) {
         </a>
         {SecondText}
       </div>
+      <button
+        className="close-banner"
+        onClick={hideBanner}
+        aria-label="Close banner"
+      >
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
     </div>
   );
 }
